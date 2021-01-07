8faf46bf4cb6186096a9d506c9ee55f3983c1927
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-07 07:35:41 -0500

    Add Egg

---
+++ src/main/java/dev/kscott/spelleggs/egg/Egg.java
@@ -0,0 +1,45 @@
+package dev.kscott.spelleggs.egg;
+
+import dev.kscott.spelleggs.spell.Spell;
+import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * The Egg class
+ */
+public class Egg {
+
+    /**
+     * The id of this Egg
+     */
+    private final @NonNull String id;
+
+    /**
+     * The ItemStack that represents this Egg
+     */
+    private final @NonNull ItemStack itemStack;
+
+    /**
+     * The Spell associated to this Egg
+     */
+    private final @NonNull Spell spell;
+
+    /**
+     * Constructs the Egg
+     *
+     * @param id {@link this#id}
+     * @param itemStack {@link this#itemStack}
+     * @param spell {@link this#spell}
+     */
+    public Egg(
+            final @NonNull String id,
+            final @NonNull ItemStack itemStack,
+            final @NonNull Spell spell
+    ) {
+        this.id = id;
+        this.itemStack = itemStack;
+        this.spell = spell;
+    }
+
+
+}
