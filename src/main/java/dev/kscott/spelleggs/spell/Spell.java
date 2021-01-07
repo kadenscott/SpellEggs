00db700748eb3fe442c0bb4a421ee6e10f13afcb
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-07 07:31:42 -0500

    Add Spell baseclass

---
+++ src/main/java/dev/kscott/spelleggs/spell/Spell.java
@@ -0,0 +1,36 @@
+package dev.kscott.spelleggs.spell;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public abstract class Spell {
+
+    /**
+     * The id of this Spell
+     */
+    protected final @NonNull String id;
+
+    /**
+     * Constructs the spell
+     *
+     * @param id {@link this#id}
+     */
+    public Spell(
+            final @NonNull String id) {
+        this.id = id;
+    }
+
+    /**
+     * This method is called when the Spell is activated
+     *
+     * @param location Location of the activation
+     * @param player   The player who activated the Spell
+     */
+    public abstract void onActivation(
+            final @NonNull Location location,
+            final @NonNull Player player
+    );
+
+
+}
