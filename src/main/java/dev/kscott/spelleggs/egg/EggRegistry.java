73f30be78e5e1a9c186c14f3a8186b96cf6b5696
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-09 15:51:23 -0500

    Add javadoc

--- src/main/java/dev/kscott/spelleggs/egg/EggRegistry.java
+++ src/main/java/dev/kscott/spelleggs/egg/EggRegistry.java
@@ -53,6 +53,9 @@ public class EggRegistry {
         this.registerEggs();
     }
 
+    /**
+     * Registers default eggs.
+     */
     private void registerEggs() {
         // Explode egg
         registerEgg(
