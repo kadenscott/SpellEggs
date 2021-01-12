f9b9d7af62c7b0231202c19d8e9adc561dd23802
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-11 20:51:28 -0500

    make class fields private

--- src/main/java/dev/kscott/spelleggs/spell/RelocationSpell.java
+++ src/main/java/dev/kscott/spelleggs/spell/RelocationSpell.java
@@ -6,18 +6,16 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.graalvm.compiler.lir.alloc.lsra.LinearScan;
 
 import java.util.Collection;
 import java.util.Random;
 
 public class RelocationSpell extends Spell {
 
-    final int localX = 7;
-    final int localZ = 7;
+    private final int localX = 7;
+    private final int localZ = 7;
 
     final @NonNull Random random;
 
@@ -28,23 +26,22 @@ public class RelocationSpell extends Spell {
 
     @Override
     public void onActivation(@NonNull Location location, @NonNull Player player) {
-        final int teleportationHeight = random.nextInt(20)+15;
-        final int localY = random.nextInt(15)+5;
+        final int teleportationHeight = random.nextInt(20) + 15;
+        final int localY = random.nextInt(15) + 5;
 
         final @NonNull World world = location.getWorld();
 
         final @NonNull Collection<Entity> entities = location.getNearbyEntities(localX, localY, localZ);
 
 
+        final int minX = location.getBlockX() - localX;
+        final int maxX = location.getBlockX() + localX;
 
-        final int minX = location.getBlockX()-localX;
-        final int maxX = location.getBlockX()+localX;
+        final int minY = location.getBlockY() - localY;
+        final int maxY = location.getBlockY() + localY;
 
-        final int minY = location.getBlockY()-localY;
-        final int maxY = location.getBlockY()+localY;
-
-        final int minZ = location.getBlockZ()-localZ;
-        final int maxZ = location.getBlockZ()+localZ;
+        final int minZ = location.getBlockZ() - localZ;
+        final int maxZ = location.getBlockZ() + localZ;
 
         for (int x = minX; x <= maxX; x++) {
             for (int y = minY; y <= maxY; y++) {
@@ -53,13 +50,13 @@ public class RelocationSpell extends Spell {
                     boolean teleport = new Random().nextInt(10) >= 3;
 
                     if (teleport) {
-                        final @NonNull Block block =  world.getBlockAt(x, y, z);
+                        final @NonNull Block block = world.getBlockAt(x, y, z);
 
                         final @NonNull BlockData blockData = block.getBlockData().clone();
 
                         block.setType(Material.AIR);
 
-                        world.getBlockAt(x, y+teleportationHeight, z).setBlockData(blockData);
+                        world.getBlockAt(x, y + teleportationHeight, z).setBlockData(blockData);
                     }
                 }
             }
